ba04683f2ea39905147db1d33bd2b1f19c40111c
gabitheiss <t_gaaby@hotmail.com>
2021-08-02 16:59:55 -0300

    Botão de voltar adicionado, setinha superior e título da página detalhes

--- app/src/main/java/com/listaanimais/DetailAnimalsActivity.kt
+++ app/src/main/java/com/listaanimais/DetailAnimalsActivity.kt
@@ -12,10 +12,18 @@ class DetailAnimalsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_animals)
 
+
+        //botão de voltar - setinha - adicionar override fun onSupportNavigateUp()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
        val animal = intent.getSerializableExtra("parametro_animal") as? Animals
 
 
         animal?.let{ myAnimal ->
+            //adicionar titulo
+            supportActionBar?.title = "${myAnimal.nomeAnimal} - ${myAnimal.corAnimal}"
+
             findViewById<ImageView>(R.id.logoImageView).apply {
                 Glide.with(context)
                     .load(myAnimal.imagem.url)
@@ -31,12 +39,11 @@ class DetailAnimalsActivity : AppCompatActivity() {
                 text = myAnimal.corAnimal
             }
         }
+    }
 
-
-
-
-
-
-
+    //para o botão voltar funcionar
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
     }
 }
